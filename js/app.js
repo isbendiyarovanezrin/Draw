82521df45ee6cafd097002977b559f4940f7d1cd
Nəzrin <isbndyrva@gmail.com>
2021-12-28 16:25:00 +0400

    Add HTML5 canvas project

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const coordinate = { x: 0, y: 0 };
+
+document.addEventListener("mousedown", start);
+document.addEventListener("mouseup", stop);
+window.addEventListener("resize", resize);
+
+resize();
+function resize() {
+  context.canvas.width = window.innerWidth;
+  context.canvas.height = window.innerHeight;
+}
+
+function getPosition(e) {
+  coordinate.x = e.clientX - canvas.offsetLeft;
+  coordinate.y = e.clientY - canvas.offsetTop;
+}
+
+function start(e) {
+  getPosition(e);
+  document.addEventListener("mousemove", draw);
+}
+
+function stop() {
+  document.removeEventListener("mousemove", draw);
+}
+
+function draw(e) {
+  context.beginPath();
+  context.moveTo(coordinate.x, coordinate.y);
+  getPosition(e);
+  context.lineTo(coordinate.x, coordinate.y);
+  context.stroke();
+  context.closePath();
+}
